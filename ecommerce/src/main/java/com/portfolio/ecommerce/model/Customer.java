d2d2c85ffcc05581df690aaed7b52d2ef03e023f
Sravan Kumar Katta <sr.katta@reply.de>
2024-11-11 16:41:27 +0100

    adding customer and orders model

---
+++ ecommerce/src/main/java/com/portfolio/ecommerce/model/Customer.java
@@ -0,0 +1,19 @@
+package com.portfolio.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+}
---
+++ ecommerce/src/main/java/com/portfolio/ecommerce/model/Order.java
@@ -0,0 +1,32 @@
+package com.portfolio.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String purchaseTime;
+    private BigInteger purchaseTimestamp;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Customer customer;
+
+}
